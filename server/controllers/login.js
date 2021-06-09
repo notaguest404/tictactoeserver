7bb840e48b66a821fe0872dc17cf2aa9a5099346
Bruno Matos <bruno.matos@feralbyte.com>
2021-06-09 22:15:55 +0100

    login fix

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -6,19 +6,18 @@ exports.post = (req, res) => {
     const email = req.body.email
     const password = req.body.password
 
-    let data = [email,password];
-
-    db.query('SELECT user.id AS id, user.username AS username, user.email AS email FROM user WHERE email = ? AND password = ?', data, function (error, results){
+    db.query('SELECT * FROM user WHERE email = ?', [email], function (error, results, fields){
         if(error) {
             res.json({
-                status:401, 
-                message: ('There are some error with query')
+                status:false, 
+                message: 'There are some error with query'
             })
-            console.log("Error:", error)
         } else {
 
             if(results.length > 0) {
-                
+
+                console.log(results)
+
                 //Check if the password is correct
                 bcrypt.compare(password, results[0].password, function (err, response) {
 
@@ -27,33 +26,18 @@ exports.post = (req, res) => {
                         console.log("Erro: ", err)
 
                     } else {
-    
-                        
-                        if ( response == false) {
-                            
-                            console.log("bcrypt falsa")
-                            //Email exists, but password is incorrect
-                            //Keep message below for security reasons by not indicating what the error was
-                            res.json({
-                                status:false,
-                                message: "Email and password does not match"
-                            });
-                            res.status(400)
 
-                        } else {
+                        res.json({
+                            user: results,
+                            code:200,
+                            message: 'Successfully authenticate'
+                        })
 
-                            console.log("bcrypt verdadeira")
-                            res.json({
-                                user: results,
-                                code:200,
-                                message: 'Successfully authenticate'
-                            })
-                        } 
                     }
                 })
 
             } else {
-                
+
                 res.json({
                     status:400,
                     message: "Please enter valid information."
